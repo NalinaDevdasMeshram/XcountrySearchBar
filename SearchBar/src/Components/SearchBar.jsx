da555b3f71713470bd7200f0f45ceea82e8e8ac6
nalina <naleenameshram85@gmail.com>
2024-07-09 13:52:38 +0530

    first commit in search bar

--- SearchBar/src/Components/SearchBar.jsx
+++ SearchBar/src/Components/SearchBar.jsx
@@ -1,12 +1,14 @@
-import { useEffect } from "react";
-
+import { useEffect, useState } from "react";
+import styles from './SearchBar.module.css'
 const SearchBar = () => {
+    const [country, setCountry] = useState([])
 
     const handleSearchBar =async()=>{
         try{
             const response = await fetch(`https://restcountries.com/v3.1/all`);
              const responseData = await response.json();
              console.log('getting datat', responseData)
+             setCountry(responseData);
         }catch{
               console.log('something went wrong')
         }
@@ -15,8 +17,15 @@ const SearchBar = () => {
         handleSearchBar()
     },[])
   return (
-    <div>
-        SearchBar
+    <div className={styles.countryCard}>
+     <input type="text" className={styles.searchBox} placeholder="Search For Search"/>
+      <div>
+        {
+           <img/>
+           <h3></h3> 
+        }
+        
+      </div>
     </div>
   )
 }
