6553fac80f500ab7d3eaca9dff467d9e4808f808
nalina <naleenameshram85@gmail.com>
2024-07-10 11:54:07 +0530

    working

--- SearchBar/src/Components/SearchBar.jsx
+++ SearchBar/src/Components/SearchBar.jsx
@@ -71,23 +71,21 @@ const SearchBar = () => {
       onChange={(e)=>setCountrySearch(e.target.value)}
       />
       
-      <div className={styles.container}>
+      <div className={styles.countryCard}>
         { 
-            
-             country.map((data)=>{
-                // console.log('filtersearchquery',filtersearchquery)
-        return(
-               <div className={styles.countryCard} key={data.cca3}>
-                <img className={styles.imgSrc} src={data.flags.png} alt={data.cca3}/>
-                <h3> {data.name.common}</h3> 
-                </div>
-                )
-             })
-          }
-        
-      </div>
-    </div>
-  )
+             country.length > 0 ? (
+              country.map((data) => (
+                  <div className={styles.container} key={data.cca3}>
+                      <img className={styles.imgSrc} src={data.flags.png} alt={data.cca3} />
+                      <h3>{data.name.common}</h3>
+                  </div>
+              ))
+          ) : (
+              <p></p>
+          )
+      }
+  </div>
+</div>
+);
 }
-
 export default SearchBar
